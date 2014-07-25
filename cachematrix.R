dfcd430346d6f59a13c63f04ea3308a11191feff
Stefan Russel <srrussel@xs4all.nl>
2014-07-25 14:50:33 +0200

    Typo and erase useless code

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,20 @@
 ###########################################################################################
-# The functions create an object "special" Matrix containing cached inverse of the matrix, 
+# The functions create an object "special" Matrix able to ontaining cached inverse of the matrix, 
 # after it is caclulated. This with help of functions below
 # 
 
 ####
 # makeCacheMatrix() with a (invertable) matrix 'x' as argument returns
-# "special" matrix, containing matrix varialbe x, list of (4) functions,
-# and variable m, intented as a cache for value inverse of matrix.
-# The first two functions (set() and get()) are used.
-# to set and get a value of matrix 'x' into "special" matrix. 
+# "special" matrix, containing matrix variable x, list of (4) functions,
+# and variable m, as a cache for value inverse of matrix.
+# The function  get() is used to get a value of matrix 'x'
+# from "special" matrix. 
 # With the last two functions, the value of the inverse of 'x' can
-# be cached (setsolve()) and retrieved (getsolve()). 
+# be cached (setsolve()) and be obtained (getsolve()). 
 
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
-
-	set <- function(y) {
-		x <<- y
-		m <- NULL
-	}
+		
 	get <-function() x
 
 	setsolve <- function(inverse) m <<- inverse 
@@ -36,7 +32,7 @@ makeCacheMatrix <- function(x = matrix()) {
 # The function cacheSolve() returns matrix that is the inverse  
 # of matrix (z$get()) from the "special" matrix which is the function argument here.
 # The function checks if a value of the inverse is cached or not, 
-# to save eventual second computation loop.
+# to save a second computation loop.
 # The function makes use of functions from list of "special" matrix 'z' ,
 # except the function set().
 # Be aware, there is no suitability test for a matrix its invertibly,
